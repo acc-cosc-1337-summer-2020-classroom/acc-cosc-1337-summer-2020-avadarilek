8f55413409b0bfcbed68ae32e6006373ad9f1556
avadarilek <avadarilek@gmail.com>
2020-06-26 11:59:00 -0500

    Homework 3: DNA (6/26/2020)

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -7,7 +7,18 @@ Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
 
+double get_gc_content(const string& dna)
+{
+    int GC_count{0};
 
+    for(char nucleotide: dna)
+    {
+        if(nucleotide =='G' || nucleotide == 'C')
+        {
+            GC_count++;
+        }
+    }
+}
 
 
 /*
@@ -15,6 +26,15 @@ Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
 
+string get_reverse_string(string dna)
+{
+    string reverse;
+    for(size_t i=dna.length(); i>0; --i)
+    {
+        reverse.push_back(dna[i-1]);
+    }
+    return reverse;
+}
 
 
 /*
@@ -27,4 +47,30 @@ b. iterate local string variable and
 c. return string
 
 */
+
+string get_dna_complement(string dna)
+{
+    string complement = get_reverse_string(dna);
+
+
+    for(size_t i=complement.length(); i> 0; --i)
+    {
+        switch(complement[i-1])
+        {
+            case 'A':
+                complement[i-1] = 'T';
+                break;
+            case 'T':
+                complement[i-1] = 'A';
+                break;
+            case 'C':
+                complement[i-1] = 'G';
+                break;
+            case 'G':
+                complement[i-1] = 'C';
+                break;
+        }
+    }
+    return complement;
+}
 
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,6 +1,10 @@
 //write include statements
+#include<iostream>
+#include"dna.h"
+#include <string>
 
 //write using statements
+using std::cout;		using std::cin;		using std::string;
 
 
 /*
@@ -12,5 +16,35 @@ user enters a y or Y.
 */
 int main() 
 {
+	int user_input;
+	string dna_string;
+	char y_or_n;
+
+	do
+	{
+		cout<<"Enter DNA string";
+		cin>>dna_string;
+
+		cout<<"Enter 1 to get GC content, or 2 to get DNA complement. \n";
+		cin>>user_input;
+		
+		
+
+		if(user_input == 1)
+		{
+			cout<<"GC content: "<<get_gc_content(dna_string);
+		}
+		else
+		{
+			cout<<"DNA complement: "<< get_dna_complement(dna_string);
+		}
+		
+
+		cout<<"Enter y to enter more DNA. If you are done, enter n";
+		cin>>y_or_n;
+		
+	}while(y_or_n == 'Y' || y_or_n == 'y');
+		
+	
 	return 0;
 }
