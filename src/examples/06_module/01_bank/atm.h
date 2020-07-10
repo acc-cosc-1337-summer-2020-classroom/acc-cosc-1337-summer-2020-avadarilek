d92e2dfe80c5d2ecfc0126c7a5436d43e4ee9c71
avadarilek <avadarilek@gmail.com>
2020-07-10 15:36:02 -0500

    random exerciese

--- src/examples/06_module/01_bank/atm.h
+++ src/examples/06_module/01_bank/atm.h
@@ -1,1 +1,24 @@
 //atm.h
+#include <vector>
+
+#include "bank_account.h"
+#ifndef ATM_H
+#define ATM_H
+
+class ATM
+{
+    public:
+    ATM(){}
+    void run();
+    private:
+    void scan_card();
+    void display_menu();
+    void set_choice();
+    void handle_transaction();
+    int choice;
+    int selected_account_index;
+    std::vector<BankAccount> accounts{BankAccount(100), BankAccount(200), BankAccount(300)};
+};
+
+
+#endif
--- src/examples/06_module/01_bank/bank_account.h
+++ src/examples/06_module/01_bank/bank_account.h
@@ -1,1 +1,46 @@
 //bank_account.h
+#include <iostream>
+
+#ifndef BRANCH_BANK_H
+#define BRANCH_BANK_H
+
+
+class BranchBank
+{
+    public:
+    BranchBank(int b): branch_balance{b}{}
+    void update_balance(int b);
+    int get_branch_balance()const{return branch_balance;}
+    private:
+    int branch_balance;
+};
+
+#endif
+
+#ifndef BANK_ACCOUNT_H//header guards
+#define BANK_ACCOUNT_H
+
+enum transaction{ DEPOSIT=1, WITHDRAW=2, DISPLAY=3};
+enum class OPTION{DEPOSIT=1, WITHDRAW=2, DISPLAY=3, EXIT=4};//c++11 
+
+class BankAccount
+{
+
+public:
+    BankAccount() : BankAccount(0){}//delegating constructor
+    explicit BankAccount(int b) : balance{b} { bank_balance += balance; } //initializer list
+    int get_balance() const {return balance;}//inline class function
+    void deposit(int amount);
+    void withdraw(int amount);
+    void set_option(OPTION o){option = o;};
+    static int get_bank_balance(){return bank_balance;}
+    friend void BranchBank::update_balance(int b);
+    friend std::ostream& operator<<(std::ostream& out, const BankAccount& account);
+    friend std::istream& operator>>(std::istream& in, BankAccount& account);
+private:
+    int balance;//lock this variable
+    OPTION option;
+    static int bank_balance;
+};
+
+#endif
